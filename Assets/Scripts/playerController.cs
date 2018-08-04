9b16f3ed60880df1625a0b2daf8ee6fb2fbe4086
Max <lautar13@hotmail.com>
2018-08-04 06:08:12 -0300

    Initial commit

---
+++ Assets/Scripts/playerController.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerController : MonoBehaviour {
+
+    private Vector3 _velocity = Vector3.zero;
+    private Vector3 _rotation = Vector3.zero;
+    private Vector3 _cameraRotation = Vector3.zero;
+    private float Speed = 5f;
+    private float MouseSensitivity = 4f;
+    public Camera cam;
+    private Rigidbody rigi;
+
+    // Use this for initialization
+    void Start ()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+        rigi = this.GetComponent<Rigidbody>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        float posX = Input.GetAxisRaw("Horizontal");
+        float posZ = Input.GetAxisRaw("Vertical");
+        float rotY = Input.GetAxisRaw("Mouse X");
+        float rotX = Input.GetAxisRaw("Mouse Y");
+
+        Vector3 moveHorizontal = transform.right * posX;
+        Vector3 moveVertical = transform.forward * posZ;
+
+        Vector3 velocity = (moveHorizontal + moveVertical).normalized * Speed;
+        _velocity = velocity;
+
+        Vector3 rotation = new Vector3(0f, rotY, 0f) * MouseSensitivity;
+        _rotation = rotation;
+
+        Vector3 cameraRotation = new Vector3(rotX, 0f, 0f) * MouseSensitivity;
+        _cameraRotation = cameraRotation;
+    }
+
+
+    void FixedUpdate()
+    {
+        Movement();
+        Rotation();
+    }
+
+    private void Movement()
+    {
+        if (_velocity != Vector3.zero)
+        {
+            rigi.MovePosition(rigi.position + _velocity * Time.fixedDeltaTime);
+        }
+    }
+    private void Rotation()
+    {
+        rigi.MoveRotation(rigi.rotation * Quaternion.Euler(_rotation));
+        if (cam != null)
+        {
+            cam.transform.Rotate(-_cameraRotation);
+        }
+    }
+}
---
+++ Assets/gunShootBullet.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class gunShootBullet : MonoBehaviour {
+
+    public Animator animationTest;
+    public int Ammo;
+    public int ammoMax;
+    public Text bulletCount;
+
+    // Update is called once per frame
+    void Start() {
+        StartCoroutine(test());
+        StartCoroutine(Shoot());
+    }
+
+    private void Update()
+    {
+        if(Ammo > ammoMax)
+        {
+            Ammo = ammoMax;
+        }
+        bulletCount.text = "Balas:" + Ammo.ToString();
+    }
+
+    private IEnumerator Shoot()
+    {
+        while (true)
+        {
+            if (Input.GetMouseButton(0) && Ammo > 0)
+            {
+                RaycastHit hit;
+                Ammo--;
+                if (Physics.Raycast(transform.position, -transform.forward, out hit, 20))
+                {
+
+                }
+                yield return new WaitForSeconds(.3f);
+            }
+            yield return new WaitForSeconds(0.001f);
+        }
+    }
+
+    IEnumerator test()
+    {
+        while (true)
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                animationTest.SetBool("Shoot", true);
+                animationTest.SetBool("Idle", false);
+                yield return new WaitForSeconds(0.5f);
+                animationTest.SetBool("Shoot", false);
+                animationTest.SetBool("Idle", true);
+            }
+            if (Input.GetKeyDown(KeyCode.R))
+            {
+                Ammo = ammoMax;
+                yield return new WaitForSeconds(.3f);
+            }
+            yield return new WaitForSeconds(0.001f);
+        }
+    }
+}
