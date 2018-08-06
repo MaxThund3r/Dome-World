5f04611e8cb9ffbb826ef120e69e83a53d273336
Max <lautar13@hotmail.com>
2018-08-05 23:04:53 -0300

    Gun shoots now
    The gun is now able to shoot

---
+++ Assets/Scripts/gunShootBullet.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class gunShootBullet : MonoBehaviour {
+
+    public Animator animationTest;
+    public int Ammo;
+    public int ammoMax;
+    public Text bulletCount;
+    public GameObject particle;
+
+    // Update is called once per frame
+    void Start() {
+        StartCoroutine(test());
+        StartCoroutine(Shoot());
+    }
+
+    private void Update()
+    {
+        if(Ammo > ammoMax)
+        {
+            Ammo = ammoMax;
+        }
+        bulletCount.text = "Balas:" + Ammo.ToString();
+
+        
+    }
+
+    private IEnumerator Shoot()
+    {
+        while (true)
+        {
+            if (Input.GetMouseButton(0) && Ammo > 0)
+            {
+                RaycastHit hit;
+                Ammo--;
+
+                Instantiate(particle, transform.position, transform.rotation * Quaternion.Euler(0,90,0));
+                if (Physics.Raycast(transform.position, transform.right, out hit, 100))
+                {
+                    Instantiate(particle, hit.point, Quaternion.LookRotation(hit.normal));
+                }
+                yield return new WaitForSeconds(.3f);
+            }
+            if (Input.GetKeyDown(KeyCode.R))
+            {
+                Ammo = ammoMax;
+                yield return new WaitForSeconds(.3f);
+            }
+            yield return new WaitForSeconds(0.001f);
+        }
+    }
+
+    IEnumerator test()
+    {
+        while (true)
+        {
+            if (Ammo > 0)
+            {
+                if (Input.GetMouseButtonDown(0))
+                {
+                    animationTest.SetBool("Shoot", true);
+                    animationTest.SetBool("Idle", false);
+                    yield return new WaitForSeconds(0.2f);
+                    animationTest.SetBool("Shoot", false);
+                    animationTest.SetBool("Idle", true);
+                }
+            }
+            yield return new WaitForSeconds(0.001f);
+        }
+    }
+}
